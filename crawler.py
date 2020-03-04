df4fe33052f75123168f1e5d30d2950f7771e925
Aleksanteri Saarela <aleksanteri.saarela@tuni.fi>
2020-03-04 23:38:37 +0200

    JSON format saving

--- crawler.py
+++ crawler.py
@@ -9,6 +9,7 @@ import time
 import csv
 import re
 import util
+import json
 
 
 # Remove double spaces, whitespaces and change everything to uppercase from ski data
@@ -23,7 +24,7 @@ def clean_ski(l):
 # Initialize chrome webdriver
 def get_chrome_driver(config):
     options = Options()
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--start-maximized")
     options.add_argument("--incognito")
     exec_path = config['chrome-webdriver']['windows']
@@ -37,25 +38,46 @@ def write_to_csv(lines):
         writer.writerows(lines)
 
 
+def write_to_json(data):
+    with open('results.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+
+
+def crawl_friends(profile, driver):
+    driver.get(f'https://steamcommunity.com/id/{profile}/friends/')
+    elements = driver.find_elements_by_class_name("friend_block_v2")
+    friends = [i.get_attribute("data-steamid") for i in elements]
+    return friends
+
+
+def get_steam_id(element, driver):
+    profile_link = element.text.split('/')[4]
+    driver.get(f'https://steamcommunity.com/id/{profile_link}?xml=1')
+    root = ET.fromstring(driver.page_source)
+    for i in root.iter('steamID64'):
+        steamID64 = i.text  # The first ID encountered
+    return steamID64
+
+
 def main():
     config = util.load_config()
     driver = get_chrome_driver(config)
+    data = {}
     # Open reviews and pick first review to start crawling
     driver.get("https://steamcommunity.com/?subsection=reviews")
     print(driver.title)
 
     # Locate submit button, click to get all results
     driver.implicitly_wait(15)
-    driver.find_element_by_class_name("apphub_Card").click()
+    reviewCards = driver.find_elements_by_class_name("apphub_Card")
+    reviewCards[1].click() # Mod as needed, some profiles are private
     element = driver.find_element_by_xpath("//a[contains(@href, 'steamcommunity.com/id')]")
     profile_link = element.text.split('/')[4]
-    driver.get(f'https://steamcommunity.com/id/{profile_link}?xml=1')
-    root = ET.fromstring(driver.page_source)
-    for i in root.iter('steamID64'):
-        steamID64 = i.text
-    print(steamID64)
+    steamID64 = get_steam_id(element, driver)
+    print("Writing to csv...")
+    data[steamID64] = crawl_friends(profile_link, driver)
+    write_to_json(data)
     time.sleep(10)
-
     # Close everything
     driver.quit()
     return 0
--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 import json
 
+
 def load_config():
     with open('config.json', 'r') as f:
         config = json.load(f)
