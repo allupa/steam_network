a79c978d94b88ed824e96354f0246348f751e430
Aleksanteri Saarela <asa@sofiadigital.fi>
2020-04-23 16:17:43 +0300

    Add scores

--- networker.py
+++ networker.py
@@ -6,18 +6,19 @@ import util
 :param G: networkx graph
 '''
 def add_review_scores(G):
+    print("Adding scores, this might take a while...")
     config = util.load_config()
     with open(config['review_scores']['file'], 'r') as f:
         scores = json.load(f)
     for k, v in scores.items():
-        G.nodes[k]['score'] = v
-    print(G.nodes[:5])
-
+        G.nodes[k][1] = v
+    return G
 
 def main():
+    print("Reading csv...")
     G = nx.read_edgelist('network.csv')
-    add_review_scores(G)
-    nx.write_gexf(G, 'steam_graph.gexf')
+    G_scored = add_review_scores(G)
+    nx.write_gexf(G_scored, 'steam_graph.gexf')
     return 0
 
 main()
