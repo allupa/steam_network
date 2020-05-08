10d2b84ce0f67dfa451c5384eebbf328177e0b5e
cikoooo <54143309+cikoooo@users.noreply.github.com>
2020-05-08 23:20:26 +0300

    Simualting spread complete

--- random_data_generator.py
+++ random_data_generator.py
@@ -1,8 +1,8 @@
 '''
-This creates a random Erdös-Rényi network, a random Barabasi-Albert network, and a random gaussian random partition network 
+This creates a random Erdös-Rényi network, a random Barabasi-Albert network, and possibly a random gaussian random partition network 
 that have the same number of nodes and about the same number of edges as the crawled data.
 
-Also, it simulates spreading in crawled and generated networks
+Generated networks are infected (number of initially infected is 2) and infected networks are saved.
 '''
 
 import numpy as np
@@ -78,15 +78,6 @@ def infect_node(G, n=1):
         G.nodes[i_0]['Infection time'] = G.graph['t']
     return G
 
-def plot(G, title):
-    color = ['r' if G.nodes[node]['Infected'] else 'g' for node in G.nodes()]
-    nx.draw(G, pos=G.graph['pos'], node_size=10, node_color=color)
-    #Jos ei toimi, niin poista nämä kommentit ja tuo pelkkä plt.title(title)
-    #if title: plt.title(title)
-    plt.title(title)
-    plt.show()
-
-
 #Tällä saat kirjoitettua halutuista tiedostoista gexf muotoisen, jolloin ne soveltuvat gephiin ja siellä näkee tartunnat. Poista kommentti functiosta infect_node
 G_ER_inf = infect_node(G_ER, n=2)
 G_BA_inf = infect_node(G_BA, n=2)
@@ -97,149 +88,3 @@ nx.write_gexf(G_ER_inf, 'random_ER_inf.gexf')
 nx.write_gexf(G_BA_inf, 'random_BA_inf.gexf')
 #nx.write_gexf(G_gaussian_inf, 'random_gaussian_inf.gexf')
 nx.write_gexf(G_crawled_inf, 'crawled_inf.gexf')
-
-
-#Infektion loppuvaiheen mallinnus
-def spread(G, p):
-    nx.set_node_attributes(G,
-                            {node: True if t == 0 else False
-                            for node, t in nx.get_node_attributes(G, 'Infection_time').items()},
-                            'Infected')
-    G.graph['t'] = 0
-    while True:
-        propagate = False
-        G.graph['t'] += 1
-        infected = [node for node in G.nodes() if G.nodes[node]['Infected']]
-        for node in infected:
-            susceptible_neighbors = [sn for sn in G.neighbors(node) if not G.nodes[sn]['Infected']]
-            propagate += len(susceptible_neighbors) > 0
-            for sn in susceptible_neighbors:
-                if np.random.random_sample() < p:
-                    G.nodes[sn]['Infected'] = True
-                    G.nodes[sn]['Infection_time'] = G.graph['t']
-        if not propagate:
-            break
-    infection_times = nx.get_node_attributes(G, 'Infection_time')
-    it_counter = Counter(infection_times.values())
-    return it_counter
-
-
-'''
-ER-network spreading
-'''
-it_counter = spread(G_ER_inf, p=0.05)
-plot(G_ER_inf, title='ER-network Infection')
-
-#Yhden datasetin kuvaajan piirtoa
-it = pd.Series(it_counter)
-it.sort_index().cumsum().plot(logy=False, marker='.')
-plt.xlabel(r'$t$', fontsize=16)
-plt.ylabel(r'$I(t)$', fontsize=16)
-plt.title('ER-network, t={}'.format(G_ER_inf.graph['t']))
-plt.show()
-
-#Monen datasetin keskiarvoa
-counter = Counter()
-for b in range(5):
-    counter += spread(G_ER_inf, p=0.05)
-total = sum(counter.values())
-for key in counter:
-    counter[key] /= float(total)
-it = pd.Series(counter)
-it.sort_index().cumsum().plot(logy=False, marker='.')
-plt.title('ER Infection Peak')
-plt.xlabel(r'$t$', fontsize=16)
-plt.ylabel(r'$I(t)$', fontsize=16)
-
-plt.show()
-
-'''
-BA-network spreading
-'''
-it_counter = spread(G_BA_inf, p=0.05)
-plot(G_BA_inf, title='BA-network Infection')
-
-#Yhden datasetin kuvaajan piirtoa
-it = pd.Series(it_counter)
-it.sort_index().cumsum().plot(logy=False, marker='.')
-plt.xlabel(r'$t$', fontsize=16)
-plt.ylabel(r'$I(t)$', fontsize=16)
-plt.title('BA-network, t={}'.format(G_BA_inf.graph['t']))
-plt.show()
-
-#Monen datasetin keskiarvoa
-counter = Counter()
-for b in range(5):
-    counter += spread(G_BA_inf, p=0.05)
-total = sum(counter.values())
-for key in counter:
-    counter[key] /= float(total)
-it = pd.Series(counter)
-it.sort_index().cumsum().plot(logy=False, marker='.')
-plt.title('BA Infection Peak')
-plt.xlabel(r'$t$', fontsize=16)
-plt.ylabel(r'$I(t)$', fontsize=16)
-
-plt.show()
-
-
-'''
-Gaussian network spreading
-
-it_counter = spread(G_gaussian_inf, p=0.05)
-plot(G_gaussian_inf, title='Gaussian-network Infection')
-
-#Yhden datasetin kuvaajan piirtoa
-it = pd.Series(it_counter)
-it.sort_index().cumsum().plot(logy=False, marker='.')
-plt.xlabel(r'$t$', fontsize=16)
-plt.ylabel(r'$I(t)$', fontsize=16)
-plt.title('Gaussian-network, t={}'.format(G_gaussian_inf.graph['t']))
-plt.show()
-
-#Monen datasetin keskiarvoa
-counter = Counter()
-for b in range(5):
-    counter += spread(G_gaussian_inf, p=0.05)
-total = sum(counter.values())
-for key in counter:
-    counter[key] /= float(total)
-it = pd.Series(counter)
-it.sort_index().cumsum().plot(logy=False, marker='.')
-plt.title('Gaussian Infection Peak')
-plt.xlabel(r'$t$', fontsize=16)
-plt.ylabel(r'$I(t)$', fontsize=16)
-
-plt.show()
-'''
-
-'''
-Crawled network spreading
-'''
-
-it_counter = spread(G_crawled_inf, p=0.05)
-plot(G_crawled_inf, title='Crawled network Infection')
-
-
-#Yhden datasetin kuvaajan piirtoa
-it = pd.Series(it_counter)
-it.sort_index().cumsum().plot(logy=False, marker='.')
-plt.xlabel(r'$t$', fontsize=16)
-plt.ylabel(r'$I(t)$', fontsize=16)
-plt.title('Crawled network, t={}'.format(G_crawled_inf.graph['t']))
-plt.show()
-
-#Monen datasetin keskiarvoa
-counter = Counter()
-for b in range(5):
-    counter += spread(G_crawled_inf, p=0.05)
-total = sum(counter.values())
-for key in counter:
-    counter[key] /= float(total)
-it = pd.Series(counter)
-it.sort_index().cumsum().plot(logy=False, marker='.')
-plt.title('Crawled Infection Peak')
-plt.xlabel(r'$t$', fontsize=16)
-plt.ylabel(r'$I(t)$', fontsize=16)
-
-plt.show()
